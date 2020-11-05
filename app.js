02cd6565d8c7d8b1eeeec526412ccf346e0e4028
Simonbelete <simonbelete@gmail.com>
2020-11-05 17:30:39 +0300

    Removing express

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-module.exports = app;
+simulator = () => {
+    var socket = require('socket.io-client')(process.env.XTRACK_URL);
+    
+}
